35dc96ac15175fbe789bfa6b30d363a1ba01f663
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2016-10-15 11:03:43 -0300

    Pequena refatoracao para compatibilizar com o que foi requisitado

--- src/test/java/com/github/saulocalixto/exercicios/CalendarioTest.java
+++ src/test/java/com/github/saulocalixto/exercicios/CalendarioTest.java
@@ -2,6 +2,8 @@ package com.github.saulocalixto.exercicios;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Só três operações são requisitadas: várias vezes o construtor;
  * o método estático transformarStringemInt e diaSemana. Curiosamente,
@@ -13,61 +15,13 @@ import org.junit.Test;
  *
  */
 public class CalendarioTest {
+
+    // 30/04/2013 é terça-feira, salvo engano, portanto, o dia da semana é 1 (em vez de 5)
+    // Deixarei essa chamada abaixo apenas para você se lembrar!! Depois remova!
     Calendario calendariocerto = new Calendario("20160902", 2016, "20130430", 5);
-    Calendario calendariodatagrande = new Calendario("520160902", 2016, "20130430", 5);
-    Calendario calendariomesconhecidoerrado = new Calendario("20161502", 2016, "20131230", 5);
-    Calendario calendariomesconhecido2errado = new Calendario("20160702", 2016, "20131530", 5);
-    Calendario calendariodiaerrado = new Calendario("20170229", 2016, "20130430", 5);
-    Calendario calendariobissextoerrado = new Calendario("20170229", -10, "20130430", 5);
-    Calendario calendariodiacoerrado = new Calendario("20170229", 2017, "20130435", 5);
 
     @Test
-    public void entradaValidaStringToInt () {
-        Calendario.transformarStringemInt();
-    }
-
-    @Test
-    public void entradaInvalidaStringToInt () {
-        Calendario.transformarStringemInt();
-    }
-    
-    @Test
-    public void entradavalidaDiaSemana () {
-        Calendario.diaSemana();
-    }
-
-    @Test
-    public void entradaInvalidaDiaSemana () {
-        Calendario.diaSemana();
-    }
-
-    @Test
-    public void semExcessaotestarExcessoes () {
-        Calendario.diaSemana();
-    }
-    
-    @Test
-    public void mesErradotestarExcessoes () {
-        Calendario.diaSemana();
-    }
-
-    @Test
-    public void mesErrado2testarExcessoes () {
-        Calendario.diaSemana();
-    }
-
-    @Test
-    public void diaErradotestarExcessoes () {
-        Calendario.diaSemana();
-    }
-
-    @Test
-    public void diaConhecidoErradotestarExcessoes () {
-        Calendario.diaSemana();
-    }
-
-    @Test
-    public void bissextoErradotestarExcessoes () {
-        Calendario.diaSemana();
+    public void DoisSetembro2016CaiNaSextaAssumindoQueArgsCorretos() {
+        assertEquals(4, Calendario.diaSemana(20160902, 2016, 20130430, 1));
     }
 }
